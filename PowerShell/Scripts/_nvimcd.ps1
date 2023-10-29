6cc18ac36f7326ff394aecc9a805e98ac7752e06
ChuseCubr <chasevillarroel@gmail.com>
2023-10-29 19:05:42 +0800

    chore: move lazyvim as separate config, remove nvimcd

--- PowerShell/Scripts/_nvimcd.ps1
+++
@@ -1,2 +0,0 @@
-nvim.exe $args
-Set-Location "$(nvim.exe --clean --headless -l `"$($env:USERSCRIPTS)/lua/_nvimcd.lua`")"
--- PowerShell/Scripts/setup/_nvim.ps1
+++ PowerShell/Scripts/setup/_nvim.ps1
@@ -1,3 +1,7 @@
-Set-Alias nvim $env:USERSCRIPTS/_nvimcd.ps1
 Set-Alias vim nvim
 Set-Alias vi nvim
+
+function lvim {
+	$env:NVIM_APPNAME=lazyvim
+	nvim $args
+}
