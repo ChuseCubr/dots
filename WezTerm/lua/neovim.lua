783202f4e3fae251d18c2e572fa952fa80a46bb7
ChuseCubr <chasevillarroel@gmail.com>
2024-10-23 16:01:09 +0800

    wezterm: update neovim colorscheme

--- WezTerm/lua/neovim.lua
+++ WezTerm/lua/neovim.lua
@@ -1,29 +1,28 @@
 local M = {}
-
 local dark_palette = {
-	blue = "#005078",
-	cyan = "#007676",
-	green = "#015825",
-	grey1 = "#0a0b10",
-	grey2 = "#1c1d23",
-	grey3 = "#2c2e33",
-	grey4 = "#4f5258",
-	magenta = "#4c0049",
-	red = "#5e0009",
-	yellow = "#6e5600",
+  blue = "#004c73",
+  cyan = "#007373",
+  green = "#005523",
+  grey1 = "#07080d",
+  grey2 = "#14161b",
+  grey3 = "#2c2e33",
+  grey4 = "#4f5258",
+  magenta = "#470045",
+  red = "#590008",
+  yellow = "#6b5300",
 }
 
 local light_palette = {
-	blue = "#9fd8ff",
-	cyan = "#83efef",
-	green = "#aaedb7",
-	grey1 = "#ebeef5",
-	grey2 = "#d7dae1",
-	grey3 = "#c4c6cd",
-	grey4 = "#9b9ea4",
-	magenta = "#ffc3fa",
-	red = "#ffbcb5",
-	yellow = "#f4d88c",
+  blue = "#a6dbff",
+  cyan = "#8cf8f7",
+  green = "#b3f6c0",
+  grey1 = "#eef1f8",
+  grey2 = "#e0e2ea",
+  grey3 = "#c4c6cd",
+  grey4 = "#9b9ea4",
+  magenta = "#ffcaff",
+  red = "#ffc0b9",
+  yellow = "#fce094",
 }
 
 local active_tab = {
